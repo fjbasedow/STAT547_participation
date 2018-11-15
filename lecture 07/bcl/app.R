9d193f1d2b6d5559d5540231ff70a137e8b193c4
Frederike Basedow <f.basedow@arcor.de>
2018-11-15 10:45:01 -0800

    lecture 8: add changes to shiny app

--- lecture 07/bcl/app.R
+++ lecture 07/bcl/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(tidyverse)
 
 bcl <- read.csv("/Users/frederikebasedow/Documents/uni/UBC Neuroscience/courses/STAT547/STAT547_participation/lecture 07/bcl/bcl-data.csv", stringsAsFactors = FALSE)
 
@@ -7,7 +8,13 @@ ui <- fluidPage(
   titlePanel("BC Liquor price app", 
              windowTitle = "BCL app"),
   sidebarLayout(
-    sidebarPanel("This text is in the sidebar."),
+    sidebarPanel(
+      sliderInput("priceInput", "Select your desired price range.",
+                  min = 0, max = 100, value = c(15, 30), pre="$"),
+      radioButtons("typeInput", "Select your alcoholic beverage type.", 
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"), 
+                   selected = "WINE")
+    ),
     mainPanel(
       plotOutput("price_hist"),
       tableOutput("bcl_data")
@@ -17,9 +24,17 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
-  output$bcl_data <- renderTable(bcl)
-
+  bcl_filtered <- reactive(bcl %>% 
+                             filter(Price < input$priceInput[2],
+                                    Price > input$priceInput[1],
+                                    Type == input$typeInput)
+  )
+  output$price_hist <- renderPlot({
+    bcl_filtered() %>% 
+      ggplot(aes(Price)) +
+      geom_histogram()
+  })
+  output$bcl_data <- renderTable(bcl_filtered()) 
 }
 
 # Run the application 
